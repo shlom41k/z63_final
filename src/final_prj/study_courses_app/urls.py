c84318d0a371a155cbed7b87e21af09a002cccfb
Sergey <ZerGey1994MU@mail.ru>
2022-04-17 10:29:50 +0300

    Added study courses functional.

--- src/final_prj/study_courses_app/urls.py
+++ src/final_prj/study_courses_app/urls.py
@@ -2,11 +2,13 @@
 
 from django.urls import path
 
-from .views import CoursesView
+from .views import CoursesView, CourseDetailView, StudyCourseView
 
 
 urlpatterns = [
     path("", CoursesView.as_view(), name="study_courses"),
+    path("<int:course_id>", CourseDetailView.as_view(), name="study_course_detail"),
+    path("study/<int:course_id>/<int:module_id>/<int:lesson_id>/<int:theme_id>", StudyCourseView.as_view(), name="study_course"),
 ]
 
 
