b5ee461027a5b62856134c42c45be13af096f758
Sergey <ZerGey1994MU@mail.ru>
2022-04-24 15:05:26 +0300

    Added ckeditor Youtube plugin

--- src/final_prj/final_prj/settings.py
+++ src/final_prj/final_prj/settings.py
@@ -169,13 +169,18 @@ CKEDITOR_CONFIGS = {
         ['Image', 'Flash', 'Table', 'HorizontalRule'],
         ['TextColor', 'BGColor'],
         ['Smiley','sourcearea', 'SpecialChar'],
-        [ 'Link', 'Unlink', 'Anchor' ],
-        [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ],
-        [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ],
-        [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],
-        [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ],
-        [ 'Maximize', 'ShowBlocks' ]
-    ],
+        ['Link', 'Unlink', 'Anchor' ],
+        ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ],
+        ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ],
+        ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],
+        ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ],
+        ['Maximize', 'ShowBlocks' ],
+        {
+            'name': 'youtube',
+            'items': ['Youtube',]
+        }
+        ],
+        'extraPlugins': 'youtube',
     }
 }
 ###################################
