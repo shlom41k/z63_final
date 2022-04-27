d9bb4838ad807f222ec5e2e0d0c90df42a3e9a62
shlom41k <ZerGey1994MU@mail.ru>
2022-04-27 07:52:19 +0300

    Added some changes

--- src/final_prj/final_prj/settings.py
+++ src/final_prj/final_prj/settings.py
@@ -190,5 +190,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shlom41k@gmail.com'
-EMAIL_HOST_PASSWORD = 'msqkhkcyhfitzqqt'
+EMAIL_HOST_USER = ''	# Put your e-mail address here
+EMAIL_HOST_PASSWORD = ''	# Put your password here
